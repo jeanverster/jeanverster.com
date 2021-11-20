7b1d5dbe313695f8726c3e869f36923c92ed54dc
Jean Verster <jeanverster0107@gmail.com>
2021-11-20 11:28:05 +0100

    chore: update text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -75,10 +75,10 @@ const Home: NextPage = () => {
       </Flex> */}
       <Page justify="center" bgPos="50% 50%" bgSize="cover" bg="transparent">
         <Container px={8} mt="12vmin" flexDir="column">
-          <Heading>Hi, I'm Jean Verster</Heading>
+          <Heading mb={6}>Hi, I'm Jean Verster</Heading>
           <Text>
             Frontend engineer passionate about building great user experiences.
-            Working with React & React Native
+            Working with React & React Native.
           </Text>
           <Divider my={4} />
           <Text>Currently @ Secfi</Text>
